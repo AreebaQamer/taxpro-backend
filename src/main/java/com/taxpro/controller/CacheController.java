4cd6ba8509644ca78fc4205da28a98c73fadab39
Areeb Qamar <aqamar@envisionmobile2025.onmicrosoft.com>
2026-04-14 18:17:22 +0500

    changes in cache controller

--- src/main/java/com/taxpro/controller/CacheController.java
+++ src/main/java/com/taxpro/controller/CacheController.java
@@ -1,10 +1,12 @@
-import javax.servlet.*;
-import javax.servlet.http.HttpServletResponse;
+package com.taxpro.controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
-public class CacheControlFilter implements Filter {
+public class CacheController implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, 
@@ -15,7 +17,18 @@ public class CacheControlFilter implements Filter {
         // Blog posts ke liye short cache
         httpResponse.setHeader("Cache-Control", "no-cache, must-revalidate");
         httpResponse.setHeader("Pragma", "no-cache");
+        httpResponse.setHeader("Expires", "0");
         
         chain.doFilter(request, response);
+    }
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization if needed
+    }
+    
+    @Override
+    public void destroy() {
+        // Cleanup if needed
     }
 }
